eb7b0d840da343d20fe80783a24afbd4a34abfdf
Ian Ku <iankuyisien@gmail.com>
2024-03-14 16:35:05 -0700

    Fetch backend data over HTTPS

--- src/main.js
+++ src/main.js
@@ -4,5 +4,5 @@ import { createApp } from 'vue'
 import App from './App.vue'
 
 const app = createApp(App)
-app.provide('api_host', 'http://data.mind.uci.edu/zigzag_backend');
+app.provide('api_host', 'https://data.mind.uci.edu/zigzag_backend');
 app.mount('#app')
